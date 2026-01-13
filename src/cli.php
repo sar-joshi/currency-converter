f8b8bcd77f440ec7158b0aff6b8c09117d9413ca
Sarthak Joshi <iamsarthakjoshi@gmail.com>
2026-01-14 10:30:45 +1100

    handle command action type in cli

--- src/cli.php
+++ src/cli.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 try {
   $args = array_slice($argv, 1);
+  $actionType = array_shift($args);
 
-  var_dump($args); # TODO: remove it later
+  switch ($actionType) {
+    case "convert":
+      # TODO
+      break;
+    case "calculate-profit":
+      # TODO
+      break;
+    default:
+      echo "Invalid Command";
+      exit(1);
+  }
 } catch (Exception $e) {
   echo $e->getMessage() . "";
   exit(1);
